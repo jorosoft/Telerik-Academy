ac06294f1fd244d89aa7ef316bc36b1ccd992886
jorosoft <jorosoftbg@gmail.com>
2016-04-03 09:20:13 +0300

    added homework C#1 - 01

---
+++ Homeworks/C# Part 1/01.IntroductionToProgramming/15.Age/Age.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+class Age
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        DateTime bDate = DateTime.ParseExact(input, "MM.dd.yyyy", null);
+        int years = DateTime.Now.Year - bDate.Year;
+        if ((DateTime.Now.Year > bDate.Year && DateTime.Now.Month < bDate.Month) || 
+            (DateTime.Now.Year > bDate.Year && DateTime.Now.Month == bDate.Month && DateTime.Now.Day < bDate.Day))
+        {
+            years--;
+        }
+        Console.WriteLine(years);
+        Console.WriteLine(years+10);        
+    }
+}
+
---
+++ Homeworks/C# Part 1/01.IntroductionToProgramming/16.LongSequence/LongSequence.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class LongSequence
+{
+    static void Main()
+    {
+        int numberOne = 0;
+        int numberTwo = -1;
+        for (int i = 0; i < 1000; i++)
+        {
+            if (i % 2 == 0)
+            {
+                numberOne += 2;
+                Console.WriteLine(numberOne);
+            }
+            else
+            {
+                numberTwo -= 2;
+                Console.WriteLine(numberTwo);
+            }
+        }
+    }
+}
