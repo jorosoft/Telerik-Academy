89050175cdc49ee70a694fe0d9ae8192b305385a
jorosoft <jorosoftbg@gmail.com>
2016-04-04 23:25:56 +0300

    added homework C# 1 - 2

---
+++ Homeworks/C# Part 1/02.DataTypesAndVariables/02.DataTypesAndVariables/13.ComparingFloats/ComparingFloats.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class ComparingFloats
+{
+    static void Main()
+    {
+        double firstNumber = double.Parse(Console.ReadLine());
+        double secondNumber = double.Parse(Console.ReadLine());
+        double eps = 0.000001;
+        if (Math.Abs(firstNumber-secondNumber) < eps)
+        {
+            Console.WriteLine("true");
+        }
+        else
+        {
+            Console.WriteLine("false");
+        }
+    }
+}
