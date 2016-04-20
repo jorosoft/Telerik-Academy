1317c522b974005915a26dde7866b217eeb326c6
jorosoft <jorosoftbg@gmail.com>
2016-04-20 18:42:55 +0300

    added practice...

---
+++ Practice/CSharpPartOne/Decoding/Decoding.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Decoding
+{
+    static void Main()
+    {
+        int salt = int.Parse(Console.ReadLine());
+        string text = Console.ReadLine();
+        int result = 0;
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == '@')
+            {
+                break;
+            }
+            else if ((text[i] >= 65 && text[i] <= 90) || (text[i] >= 97 && text[i] <= 122))
+            {
+                result = text[i] * salt + 1000;
+            }
+            else if (text[i] >= 48 && text[i] <= 57)
+            {
+                result = text[i] + salt + 500;
+            }
+            else
+            {
+                result = text[i] - salt;
+            }
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("{0:F2}", result / 100.00);
+            }
+            else
+            {
+                Console.WriteLine("{0}", result * 100);
+            }
+        }
+    }
+}
---
+++ Practice/CSharpPartOne/ThreeNumbers/ThreeNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class ThreeNumbers
+{
+    static void Main()
+    {
+        int[] numbers = new int[3];
+        numbers[0] = int.Parse(Console.ReadLine());
+        numbers[1] = int.Parse(Console.ReadLine());
+        numbers[2] = int.Parse(Console.ReadLine());
+        Array.Sort(numbers);
+        Console.WriteLine(numbers[2]);
+        Console.WriteLine(numbers[0]);
+        Console.WriteLine("{0:F2}", (numbers[0] + numbers[1] + numbers[2]) / 3.00);
+    }
+}
